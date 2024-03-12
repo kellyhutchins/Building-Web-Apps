eafd00ad26b3a2365b28b57d77e1e0824a7a7194
Kelly Hutchins <khutchins@esri.com>
2024-03-11 19:21:24 -0700

    chore: Add additional demos

--- webmap-modify/main.js
+++ webmap-modify/main.js
@@ -1,3 +1,33 @@
 import "./assets/main.css";
+import { defineCustomElements as defineCalciteElements } from "@esri/calcite-components/dist/loader";
 import { defineCustomElements } from "@arcgis/map-components/dist/loader";
+
+defineCalciteElements(window, {
+  resourcesUrl: "https://js.arcgis.com/calcite-components/2.5.1/assets",
+});
 defineCustomElements(window, { resourcesUrl: "https://js.arcgis.com/map-components/4.29/assets" });
+
+const mapElement = document.querySelector('arcgis-map');
+
+mapElement.addEventListener('arcgisViewReadyChange', event => { 
+    mapElement.constraints = {
+        geometry: { 
+          spatialReference:{
+            latestWkid:3857,
+            wkid:102100
+          },
+          type: "extent",
+          xmin: -14014396.04,
+          ymin: 5569160.95,
+          xmax: -12685859.33,
+          ymax: 6350653.12
+        },
+        minScale: 5000000, 
+        maxScale: 0
+      };
+
+    const definitionExpression = "STATE='WA'";
+    mapElement.map.layers.forEach(layer=>{
+        layer.definitionExpression = definitionExpression;
+    });
+}); 
